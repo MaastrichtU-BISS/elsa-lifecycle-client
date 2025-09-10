b95a4dba16e74678b8bfd40012e227e5a248006c
CvdLans <93982741+CvdL-UM@users.noreply.github.com>
2025-09-10 17:46:57 +0200

    refactor: Check whether file_upload is required instead of implicitly through url and add checked_done

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -2,8 +2,7 @@ export function isRecommendationDone(
   rec: Recommendation | undefined,
   answer: RecommendationAnswer | undefined
 ): boolean {
-  if(!rec || !answer) {
-    return false;
-  }
-  return !(rec.Tool?.form && !answer.form) && !(rec.Tool?.url && !answer.file);
+  if (!rec || !answer) return false;
+
+  return (!!rec.Tool?.form && !answer.form) || (rec.Tool?.file_upload && !answer.file) || answer.checked_done;
 }
