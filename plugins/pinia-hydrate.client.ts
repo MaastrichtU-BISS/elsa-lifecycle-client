68be740592de895d75d1a3cf77a83be018ccf4b9
Milan Dirks <159008288+Cosmogos@users.noreply.github.com>
2026-05-07 09:42:41 +0200

    feat: remove unused pinia-hydrate plugin for lifecycle store

--- plugins/pinia-hydrate.client.ts
+++
@@ -1,8 +0,0 @@
-import { useLifecycleStore } from '~/stores/lifecycle'
-
-export default defineNuxtPlugin(() => {
-  const store = useLifecycleStore()
-  if (import.meta.client) {
-    store.hydrateFromLocalStorage()
-  }
-})
