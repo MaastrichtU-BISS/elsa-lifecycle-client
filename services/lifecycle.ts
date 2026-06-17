4cc518a1b65555d016ce1a5c2e96785d9807987b
carloseam94 <carloseam94@gmail.com>
2026-06-17 11:05:42 +0200

    remove: unnecesary functions from lifecycle service

--- services/lifecycle.ts
+++ services/lifecycle.ts
@@ -1,6 +1,5 @@
 import { BaseService } from "./base";
 import type { Lifecycle } from "~/utils/types";
-import type { LastLifecycle } from "~/utils/types";
 
 export class LifecycleService extends BaseService {
   async getAllLifecycles(): Promise<Lifecycle[]> {
@@ -10,7 +9,7 @@ export class LifecycleService extends BaseService {
       });
       return response as Lifecycle[];
     } catch (error) {
-      throw new Error(`Failed to fetch reflection: ${error}`);
+      throw new Error(`Failed to fetch lifecycle: ${error}`);
     }
   }
 
@@ -23,64 +22,6 @@ export class LifecycleService extends BaseService {
       return response as Lifecycle;
     } catch (error) {
       throw new Error(`Failed to fetch lifecycle: ${error}`);
-    }
-  }
-
-  async generatePDFById(id: number): Promise<Blob> {
-    try {
-      const response = await $fetch(`${this.url}/lifecycles/${id}/pdf`, {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${this.token}`,
-        },
-        responseType: "blob",
-      });
-
-      return response as Blob;
-    } catch (error) {
-      throw new Error(`Failed to generate PDF: ${error}`);
-    }
-  }
-
-  async generatePDFByIdForReflection(
-    id: number,
-    reflectionId: number,
-  ): Promise<Blob> {
-    try {
-      const response = await $fetch(
-        `${this.url}/lifecycles/${id}/pdf?reflectionId=${reflectionId}`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${this.token}`,
-          },
-          responseType: "blob",
-        },
-      );
-
-      return response as Blob;
-    } catch (error) {
-      throw new Error(`Failed to generate PDF: ${error}`);
-    }
-  }
-
-  async getLastLifecycleForUser(): Promise<LastLifecycle | null> {
-    try {
-      const response = await $fetch(
-        `${this.url}/reflectionAnswers/latest-lifecycle`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${this.token}`,
-          },
-        },
-      );
-      return response as LastLifecycle;
-    } catch (error) {
-      if (error instanceof Error && error.message.includes("404")) {
-        return null;
-      }
-      throw new Error(`Failed to fetch last lifecycle: ${error}`);
     }
   }
 }
