0de3f01fc51b4dbe28fec45af6f2fc76f9bc4f7b
carloseam94 <carloseam94@gmail.com>
2026-06-17 16:36:23 +0200

    feat: create journals  based on a fixed lifecycle. show on table

--- pages/journals/index.vue
+++ pages/journals/index.vue
@@ -2,13 +2,14 @@
 import { LifecycleService } from '~/services/lifecycle';
 import { JournalService } from '~/services/journal';
 import type { Lifecycle, Journal } from '~/utils/types';
+import JournalsTable from '~/components/lifecycle/JournalsTable.vue';
 
 const auth = useAuthStore();
 
 const config = useRuntimeConfig();
 const lifecycleService = new LifecycleService(config.public.apiBase as string);
 const journalService = new JournalService(config.public.apiBase as string);
-const journals = ref<Journal[]>([]);
+const journals = ref<Journal[]>();
 
 onMounted(async () => {
     if (auth.token) {
@@ -18,17 +19,25 @@ onMounted(async () => {
     }
 });
 
+watch(() => auth.token, async (newToken) => {
+    if (newToken) {
+        lifecycleService.setToken(newToken);
+        journalService.setToken(newToken);
+        journals.value = await journalService.getAllJournals();
+    } else {
+        journals.value = [];
+    }
+});
+
 
 const createNewJournal = async () => {
     try {
 
         const lifecycles: Lifecycle[] = await lifecycleService.getAllLifecycles();
-
-        console.log('Lifecycles:', lifecycles);
-
-
         const newJournal = await journalService.createJournal({ title: 'New Journal', lifecycleId: lifecycles[0].id });
-        journals.value.push(newJournal);
+        journals.value?.push(newJournal);
+
+        console.log('New journal created:', newJournal);
     } catch (error) {
         console.error('Error creating new journal:', error);
     }
@@ -40,12 +49,21 @@ const createNewJournal = async () => {
         <section>
             <h1 class="text-2xl font-bold mt-8 mb-4 text-center">My Journals</h1>
             <template v-if="auth.token">
-                <button class="mt-4" @click="createNewJournal">Create a new Journal</button>
-                <!-- <JournalTable :journals="journals" /> -->
+                
+                <template v-if="journals && journals.length > 0">
+                    <JournalsTable :journals="journals" />
+                </template>
+                <template v-else>
+                    <UAlert icon="i-lucide-info" color="warning" variant="subtle" title="You have no journals yet"
+                        description="Create your first journal to get started." class="mb-4" />
+                </template>
+
+                <UButton class="mt-4" color="primary" icon="i-lucide-plus" size="lg" @click="createNewJournal">Create a new Journal</UButton>
             </template>
             <template v-else>
                 <UAlert icon="i-lucide-info" color="warning" variant="subtle" title="Please log in to continue"
-                    description="You need to be logged in order to see your journals or create a new one." class="mb-4" />
+                    description="You need to be logged in order to see your journals or create a new one."
+                    class="mb-4" />
             </template>
         </section>
     </div>
